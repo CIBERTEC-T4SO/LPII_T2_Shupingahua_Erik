06ec16b39df88f45533649dfa388ac02055f2eb6
Erik Shupingahua <i201120094@cibertec.edu.pe>
2025-06-15 01:52:28 -0500

    Agregando Entidades

---
+++ src/main/java/org/cibertec/soluciont2/entity/ProveedorEntity.java
@@ -0,0 +1,31 @@
+package org.cibertec.soluciont2.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "provider")
+public class ProveedorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "doc_number")
+    private String doc;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private String telefono;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "idpais")
+    private CountryEntity pais;
+
+}
