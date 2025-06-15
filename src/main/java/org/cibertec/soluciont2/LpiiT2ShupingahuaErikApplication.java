9c5de8fa86cc3415d25b7cff963b8bcad5421130
Erik Shupingahua <i201120094@cibertec.edu.pe>
2025-06-15 01:50:14 -0500

    Proyecto Base

---
+++ src/main/java/org/cibertec/soluciont2/LpiiT2ShupingahuaErikApplication.java
@@ -0,0 +1,13 @@
+package org.cibertec.soluciont2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiT2ShupingahuaErikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiT2ShupingahuaErikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cibertec/soluciont2/LpiiT2ShupingahuaErikApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cibertec.soluciont2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiiT2ShupingahuaErikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
